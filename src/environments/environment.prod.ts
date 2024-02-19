90c27be8b90b8dbe4d9a424c679eb26270f1d02b
Mariana <75488872+marianapequeno@users.noreply.github.com>
2024-02-19 09:45:14 -0300

    Criação dos arquivos de variáveis de ambiente

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+//Arquivo onde estão as variáveis de ambiente DE PRODUÇÃO
+
+export const environment = {
+  production: true,
+  API_URL: "http://localhost:3333" //Em um projeto real, add a url de deploy da aplicação
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+// Arquivo onde estão as variáveis de ambiente local (DE DESENVOLVIMENTO)
+
+export const environment = {
+  production: false,
+  API_URL: "http://localhost:3333"
+};
