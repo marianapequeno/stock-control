f539f65abc0ea42a5b30c3f69b2b018b3f6a2f2f
Mariana <75488872+marianapequeno@users.noreply.github.com>
2024-02-20 16:09:38 -0300

    Implementação da funcionalidade de logout

--- src/app/shared/components/toolbar-navigation/toolbar-navigation.component.ts
+++ src/app/shared/components/toolbar-navigation/toolbar-navigation.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-toolbar-navigation',
   templateUrl: './toolbar-navigation.component.html',
-  styleUrls: ['./toolbar-navigation.component.scss']
+  styleUrls: ['./toolbar-navigation.component.scss'],
 })
 export class ToolbarNavigationComponent {
+  constructor(private cookie: CookieService, private router: Router) {}
+
+
+  //Fazer logout do sistema
+  handleLogout(): void {
+    this.cookie.delete('USER_INFO'); //Deleta o token JWT dos cookies
+    void this.router.navigate(['/home']); //Redireciona para a tela de login, a rota '/home'
+  }
+
 
 }
