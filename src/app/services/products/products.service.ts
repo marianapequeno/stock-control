90a8077f7e92c292cda4a0a79849024eb762af41
Mariana <75488872+marianapequeno@users.noreply.github.com>
2024-02-21 14:47:30 -0300

    Implementação do serviço de get all  products

---
+++ src/app/services/products/products.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { CookieService } from 'ngx-cookie-service';
+import { Observable, map } from 'rxjs';
+import { GetAllProductsResponse } from './../../models/interfaces/products/request/GetAllProductsResponse';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+  private API_URL = environment.API_URL;
+  private JWT_TOKEN = this.cookie.get('USER_INFO');
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      Autorization: `Bearer ${this.JWT_TOKEN}`
+    }),
+  }
+
+  constructor(private http: HttpClient, private cookie: CookieService) { }
+
+  GetAllProducts(): Observable<Array<GetAllProductsResponse>> {
+    return this.http.get<Array<GetAllProductsResponse>>(
+      `${this.API_URL}/products`, this.httpOptions
+    ).pipe(
+      map((product) => product.filter((data) => data?.amount > 0))
+    );
+  }
+
+
+
+}
