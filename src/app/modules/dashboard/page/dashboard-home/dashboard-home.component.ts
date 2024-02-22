a8f6f5808c6b5b7b129258e3b40bf15d12f99b49
Mariana <75488872+marianapequeno@users.noreply.github.com>
2024-02-22 15:32:38 -0300

    Implementação do gráfico de produtos em estoque

--- src/app/modules/dashboard/page/dashboard-home/dashboard-home.component.ts
+++ src/app/modules/dashboard/page/dashboard-home/dashboard-home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ChartData, ChartOptions } from 'chart.js';
 import { MessageService } from 'primeng/api';
-import { Observable, Subject, takeUntil } from 'rxjs';
+import { Observable, Subject, elementAt, takeUntil } from 'rxjs';
 import { GetAllProductsResponse } from 'src/app/models/interfaces/products/request/GetAllProductsResponse';
 import { ProductsService } from 'src/app/services/products/products.service';
 import { ProductsDataTransferService } from 'src/app/shared/services/products/products-data-transfer.service';
@@ -14,11 +15,13 @@ export class DashboardHomeComponent implements OnInit, OnDestroy {
   private destroy$ = new Subject<void>();
   public productList: Array<GetAllProductsResponse> = [];
   public ObservableTest$ = new Observable();
+  public productsChartDatas!: ChartData;
+  public productsCharOptions!: ChartOptions;
 
   constructor(
     private productService: ProductsService,
     private messageService: MessageService,
-    private productsDtService: ProductsDataTransferService,
+    private productsDtService: ProductsDataTransferService
   ) {}
 
   ngOnInit(): void {
@@ -26,29 +29,90 @@ export class DashboardHomeComponent implements OnInit, OnDestroy {
   }
 
   getProductsData(): void {
-    this.productService.getAllProducts()
-    .pipe(takeUntil(this.destroy$))
-    .subscribe({
-      next: (response) => {
-        if (response.length > 0) {
-          this.productList = response;
-          this.productsDtService.setProductsDatas(this.productList);
-        }
-      }, error: (err) => {
-        console.log(err);
-        this.messageService.add({
-          severity: 'error',
-          summary: 'Erro',
-          detail: 'Erro ao buscar produtos',
-          life: 2500
-        });
-      }
-    })
-  }
+    this.productService
+      .getAllProducts()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (response) => {
+          if (response.length > 0) {
+            this.productList = response;
+            this.productsDtService.setProductsDatas(this.productList);
+            this.setProductsChartConfig();
+          };
+        },
+        error: (err) => {
+          console.log(err);
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Erro',
+            detail: 'Erro ao buscar produtos',
+            life: 2500,
+          });
+        },
+      });
+  };
+
+  setProductsChartConfig(): void {
+    if (this.productList.length > 0) {
+      const documentStyle = getComputedStyle(document.documentElement);
+      const textColor = documentStyle.getPropertyValue('--text-color');
+      const textColorSecondary = documentStyle.getPropertyValue(
+        '--textcolor-secondary'
+      );
+      const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
+
+      this.productsChartDatas = {
+        labels: this.productList.map((element) => element.name),
+        datasets: [
+          {
+            label: 'Quantidade',
+            backgroundColor: documentStyle.getPropertyValue('--indigo-400'),
+            borderColor: documentStyle.getPropertyValue('--indigo-400'),
+            hoverBackgroundColor:
+              documentStyle.getPropertyValue('--indigo-500'),
+            data: this.productList.map((element) => element?.amount),
+          },
+        ],
+      };
+
+      this.productsCharOptions = {
+        maintainAspectRatio: false,
+        aspectRatio: 0.8,
+        plugins: {
+          legend: {
+            labels: {
+              color: textColor,
+            },
+          },
+        },
+
+        scales: {
+          x: {
+            ticks: {
+              color: textColorSecondary,
+              font: {
+                weight: 500,
+              },
+            },
+            grid: {
+              color: surfaceBorder,
+            },
+          },
+          y: {
+            ticks: {
+              color: textColorSecondary,
+            },
+            grid: {
+              color: surfaceBorder,
+            },
+          },
+        },
+      };
+    };
+  };
 
   ngOnDestroy(): void {
     this.destroy$.next();
     this.destroy$.complete();
-  }
-
-}
+  };
+};
