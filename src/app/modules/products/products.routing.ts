c85c96076ea3b0ad92ead9d2f827b645d7d0d651
Mariana <75488872+marianapequeno@users.noreply.github.com>
2024-02-22 15:55:49 -0300

    Criação do module, route e component da tela de Produtos

---
+++ src/app/modules/products/products.routing.ts
@@ -0,0 +1,6 @@
+import { Routes } from "@angular/router";
+import { ProductsHomeComponent } from "./page/products-home/products-home.component";
+
+export const PRODUCTS_ROUTES: Routes = [
+  { path: '', component: ProductsHomeComponent }
+]
