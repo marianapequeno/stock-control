0d88c60eddaa24b9451bbfa2725a4fe710fe8fb6
Mariana <75488872+marianapequeno@users.noreply.github.com>
2024-02-28 15:05:01 -0300

    Incluindo a categoria para permitir alteração dela ao editar um produto existente

--- src/app/modules/products/components/product-form/product-form.component.ts
+++ src/app/modules/products/components/product-form/product-form.component.ts
@@ -45,11 +45,13 @@ export class ProductFormComponent implements OnInit, OnDestroy {
     price: ['', Validators.required],
     description: ['', Validators.required],
     amount: [0, Validators.required],
+    category_id: ['', Validators.required],
   });
 
   public addProductAction = ProductEvent.ADD_PRODUCT_EVENT;
   public editProductAction = ProductEvent.EDIT_PRODUCT_EVENT;
   public saletProductAction = ProductEvent.SALE_PRODUCT_EVENT;
+  public renderDropdown = false;
 
   constructor(
     private categoriesService: CategoriesService,
@@ -63,15 +65,12 @@ export class ProductFormComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.productAction = this.ref.data;
-    if (
-      this.productAction?.event?.action === this.editProductAction &&
-      this.productAction?.productsDatas
-    ) {
-      this.getProductSelectedDatas(this.productAction?.event?.id as string);
-    }
+
     this.productAction?.event?.action === this.saletProductAction &&
       this.getProductDatas();
+
     this.getAllCategories();
+    this.renderDropdown = true;
   }
 
   getAllCategories(): void {
@@ -82,6 +81,12 @@ export class ProductFormComponent implements OnInit, OnDestroy {
         next: (response) => {
           if (response.length > 0) {
             this.categoriesDatas = response;
+            if (
+              this.productAction?.event?.action === this.editProductAction &&
+              this.productAction?.productsDatas
+            ) {
+              this.getProductSelectedDatas(this.productAction?.event?.id as string);
+            }
           }
         },
       });
@@ -137,6 +142,7 @@ export class ProductFormComponent implements OnInit, OnDestroy {
         description: this.editProductForm.value.description as string,
         product_id: this.productAction?.event?.id,
         amount: this.editProductForm.value.amount as number,
+        category_id: this.editProductForm.value.category_id as string,
       };
       this.productService
         .editProduct(requestEditProduct)
@@ -180,6 +186,7 @@ export class ProductFormComponent implements OnInit, OnDestroy {
           price: this.productSelectedDatas?.price,
           amount: this.productSelectedDatas?.amount,
           description: this.productSelectedDatas?.description,
+          category_id: this.productSelectedDatas?.category?.id,
         });
       }
     }
